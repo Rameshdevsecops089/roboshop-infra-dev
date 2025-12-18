3542c713a3c89a6529d66e0907e5ee44f2e74a48
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-12-18 23:15:56 +0530

    Added roboshop infra code

---
+++ 10-sg/main.tf
@@ -0,0 +1,43 @@
+module "frontend" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/Rameshdevsecops089/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = var.frontend_sg_name
+    sg_description = var.frontend_sg_description
+    vpc_id = local.vpc_id
+}
+
+module "bastion" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/Rameshdevsecops089/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = var.bastion_sg_name
+    sg_description = var.bastion_sg_description
+    vpc_id = local.vpc_id
+}
+
+module "backend_alb" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/Rameshdevsecops089/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = "backend-alb"
+    sg_description = "for backend alb"
+    vpc_id = local.vpc_id
+}
+
+# bastion accepting connections from my laptop
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] 
+  security_group_id = module.mongodb.sg_id
+}
+
